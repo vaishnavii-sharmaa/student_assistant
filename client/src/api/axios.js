81824b8c7b41b877939d6017fffe5d559ac5cb15
vaishnavi sharma <vaishnavi.sharma_cs23@gla.ac.in>
2026-07-31 18:31:12 +0530

    Use deployed backend URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://student-assistant-vdct.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
